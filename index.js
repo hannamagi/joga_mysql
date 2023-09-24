28deff5952378e80881f9103e154238dc5d243d8
hannamagi <hanna.magi14@gmail.com>
2023-09-24 19:08:44 +0300

    Added author name into single article

--- index.js
+++ index.js
@@ -55,7 +55,21 @@ app.get('/article/:slug', (req, res) => {
             article: article
         })
     })
-})
+});
+
+app.get("/author/:author_id", (req, res) => {
+    let query = `SELECT article.*, author.name AS author_name FROM article join author on article.author_id=author.id where article.author_id = '${req.params.author_id}'`;
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        let author_name = result[0].author_name
+        res.render("author", {
+            articles: articles,
+            author_name: author_name
+        })
+    })
+});
 
 app.get("/", (req, res) => {
     let query = 'SELECT * FROM article';
