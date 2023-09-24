42caf09c474bd2629a30bf7e9feba3c9e9c71c6d
hannamagi <hanna.magi14@gmail.com>
2023-09-24 16:02:04 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ con.connect(function(err) {
     console.log('Connected to joga_mysql db');
 })
 
-app.listen(3000, () => {
-    console.log('App is starting at http://localhost:3000')
+app.get("/", (req, res) => {
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        res.render('index.hbs', {
+            articles: articles
+        })
+    })
+});
+
+app.listen(4000, () => {
+    console.log('App is starting at http://localhost:4000')
 })
